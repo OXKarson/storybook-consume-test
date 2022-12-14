c2f6125d39f2e31e3fc597bc2ace7f6d60de50a8
Ott, Carson <carson.ott@chase.com>
2022-12-14 11:05:26 -0500

    update

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,28 @@
 const path = require("path");
 
-module.exports = {
+module.exports = (env = {}) => ({
+  mode: env.production ? "production" : "development",
+  resolve: {
+    extensions: [".ts", ".tsx", ".js", ".html"],
+    modules: ["node_modules", path.resolve(__dirname, "src")],
+  },
+  // eslint-disable-next-line compat/compat
   entry: "./src/index.ts",
+  devtool: "source-map",
+  externals: {
+    react: "react",
+  },
+  output: {
+    library: {
+      type: env.commonjs ? "commonjs" : "module",
+    },
+    module: true,
+    filename: `[name].js`,
+    path: path.resolve(__dirname, "dist"),
+  },
+  experiments: {
+    outputModule: true,
+  },
   module: {
     rules: [
       {
@@ -16,11 +37,4 @@ module.exports = {
       },
     ],
   },
-  resolve: {
-    extensions: [".tsx", ".ts", ".js"],
-  },
-  output: {
-    filename: "bundle.js",
-    path: path.resolve(__dirname, "dist"),
-  },
-};
+});
